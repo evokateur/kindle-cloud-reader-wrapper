b9d966fc1cb3767e18cbf6ee744d60a658f98db5
ウエスリー <wesley@cipolla.evokateur.net>
2025-05-21 16:57:40 -0700

    initial commit of runnable app

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, nativeImage } = require('electron');
+const path = require('path');
+
+function createWindow()
+{
+    const icon = nativeImage.createFromPath(path.join(__dirname, 'assets/icon.png'));
+
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        icon,
+        autoHideMenuBar: true,
+        title: "Kindle Cloud Reader",
+        webPreferences: {
+            sandbox: true,
+            contextIsolation: true,
+        },
+    });
+
+    win.loadURL("https://read.amazon.com");
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
